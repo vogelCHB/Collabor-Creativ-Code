9acdda9fe58e859db675b6f16ea9987aca3f6e32
vogelCHB <115561645+vogelCHB@users.noreply.github.com>
2024-06-03 20:57:27 +0200

    Add files via upload

---
+++ Sprint_04/Sprint_02/Sprint_04_Variante_08/sketch.js
@@ -0,0 +1,62 @@
+let rows = 40;
+let cols = 40;
+let grid = [];
+
+function setup() {
+  createCanvas(400, 400);
+
+  for (let i = 0; i < rows; i++) {
+    grid[i] = [];
+    for (let j = 0; j < cols; j++) {
+      let x = j * 10
+      let y = i * 10
+      grid[i][j] = new PointObject(x, y);
+    }
+  }
+}
+
+function draw() {
+  background(220);
+
+  // Anzeigen jedes Punkt-Objekts
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      grid[i][j].display();
+    }
+  }
+}
+
+class PointObject {
+  constructor(x, y) {
+    this.x = x
+    this.y = y;
+  }
+
+  display() {
+
+    fill(0); // Farbe des Punkts (schwarz)
+    noStroke(); // Keine Umrandung
+
+    let noiselevel1 = 8
+    let noiselevel2 = 4
+
+    let p = (map(this.x, 0, 400, noiselevel1, noiselevel2));
+    let q = (map(this.y, 0, 400, noiselevel1, noiselevel2));
+
+    // let s = (noise(frameCount * 0.01)) * q;
+    // textSize(s);
+    // text("ei", this.x, this.y); // Punkt zeichnen
+
+
+    let s = (noise(frameCount * 0.01) * p * q);
+    textSize(s);
+    text("ei", this.x, this.y); // Punkt zeichnen
+
+
+
+
+
+
+
+  }
+}
